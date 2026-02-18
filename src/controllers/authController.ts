50062107c6ee51737d9338d318be88213f849a4e
AshishRajx7 <ashishrajcr7@gmail.com>
2026-02-18 18:23:50 +0530

    addded loginschema

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
-import { registerSchema } from "../validators/authValidator";
+import { registerSchema,loginSchema} from "../validators/authValidator";
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -23,3 +23,27 @@ export const register = async (req: Request, res: Response) => {
     return res.status(400).json({ message: "Invalid request" });
   }
 };
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const data = loginSchema.parse(req.body);
+    const user = await User.findOne({ email: data.email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const isMatch = await bcrypt.compare(data.password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET!,
+      { expiresIn: "24h" }
+    );
+
+    return res.json({ token });
+  } catch (err) {
+    return res.status(400).json({ message: "Invalid request" });
+  }
+};
+
--- src/validators/authValidator.ts
+++ src/validators/authValidator.ts
@@ -5,3 +5,9 @@ export const registerSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8)
 });
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string()
+});
+
