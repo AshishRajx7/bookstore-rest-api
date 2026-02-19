bf3a2c31808b99a72504ce2ff1f2f2833c572967
AshishRajx7 <ashishrajcr7@gmail.com>
2026-02-20 02:08:50 +0530

    completed teh CRUD

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,11 +1,50 @@
 import { Request, Response } from "express";
 import Book from "../models/Book";
-
+// Create a new book
 export const createBook = async (req: Request, res: Response) => {
   try {
     const book = await Book.create(req.body);
-    res.status(201).json(book);
-  } catch {
-    res.status(400).json({ message: "Invalid data" });
+    return res.status(201).json(book);
+  } catch (err) {
+    return res.status(400).json({ message: "Invalid data" });
   }
 };
+//Read all books
+export const getBooks = async (req: Request, res: Response) => {
+  try {
+    const books = await Book.find();
+    return res.json(books);
+  } catch (err) {
+    return res.status(500).json({ message: "Error finding books" });
+  }
+};
+// Read one book by id
+export const getBookById = async (req: Request, res: Response) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.json(book);
+  } catch (err) {
+    return res.status(400).json({ message: "Invalid id" });
+  }
+};
+//update a book by id
+export const updateBook = async (req: Request, res: Response) => {
+  try {
+    const book = await Book.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.json(book);
+  } catch (err) {
+    return res.status(400).json({ message: "Invalid id or data" });
+  }
+};
+
+
--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { createBook } from "../controllers/bookController";
 import { protect } from "../middleware/authMiddleware";
-
+import {getBooks} from "../controllers/bookController";
+import {getBookById} from "../controllers/bookController";
+import {updateBook} from "../controllers/bookController";
 const router = Router();
 
 router.post("/", protect, createBook);
+router.get("/", getBooks);
+router.get("/:id", getBookById);
+router.put("/:id", protect, updateBook);
 
 export default router;
